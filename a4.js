6afaa01a8b9fc9dead44a22e0ff230eb6d1b9c6d
Conner Knutson <cknutson@smu.edu>
2015-02-09 13:06:57 -0600

    assignment 4 put in functions

--- a4.js
+++ a4.js
@@ -1,8 +1,16 @@
-document.body.style.background = 'red';
-alert(document.title);
-var address = "cknutson@smu.edu";
-var regx = new RegExp("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$");
-if(address.match(regx) != null)
-        alert(address + " is a valid email");
-else      
-        alert(address + " is not a valid email");
+function backColor {
+	document.body.style.background = 'red';
+}
+
+function titleAlert {
+	alert(document.title);
+}
+
+function emailVal {
+	var address = "cknutson@smu.edu";
+	var regx = new RegExp("^[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$");
+	if(address.match(regx) != null)
+        	alert(address + " is a valid email");
+	else      
+        	alert(address + " is not a valid email");
+}
